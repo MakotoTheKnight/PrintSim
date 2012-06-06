a086ce6610d60c393417695830f64baf1d058448
Jason Black <makotothedragon@gmail.com>
2012-06-05 19:54:04 -0600

    Initial files uploaded (no tests)

---
+++ src/Clock.java
@@ -0,0 +1,286 @@
+/*  Clock.java:
+    This class is referred to in this program as an almost entirely static class.
+    Most everything that it does doesn't require that an object of type Clock be created, but for
+    ease of use and reference, one is usually created anyway in main.
+    Written by Jason Black, 29 October 2008
+*/
+
+
+import java.util.Scanner; // Used for the pause() method
+import java.io.*; // Used to write the output file (summations)
+
+public class Clock implements ClockEvent {
+
+    private static double randomizer = Math.random();
+	private static int currentTick, currentJob, jobCount = 3, maxJobLength = 6;
+	private String generatedJobInformation;
+	private static int MAX_TIME_ALLOT, WAIT_TIME; // WAIT_TIME used to flag impatient jobs
+	private static String[] uniquePaper, uniqueInk;
+	private static boolean priorities = false, bias = false; // Bias used to represent 70% B/W job/printers
+
+	public Clock() {
+
+		this(100); // maximum time allotted for the simulation
+
+	}
+
+	public Clock (int jobs, int maxlength, int clock) {
+		this(clock);
+		jobCount = jobs;
+		maxJobLength = maxlength;
+	}
+
+	public Clock(boolean priority) {
+		this();
+		priorities = priority; // Specify if job is a priority-type job
+	}
+
+	public Clock(int maxSimTime, boolean biased) {
+		this(maxSimTime);
+		bias = biased;
+	}
+
+	public Clock(boolean priority, int patienceLevel) {
+		this(priority);
+		WAIT_TIME = patienceLevel;
+	}
+
+	public Clock(int maxSimTime) {
+
+		MAX_TIME_ALLOT = maxSimTime;
+		currentTick = 0;
+		currentJob = 0;
+		generatedJobInformation = "";
+		uniquePaper = new String[3];
+		uniqueInk = new String[2];
+		for(int i = 0; i < uniquePaper.length; i++) {
+			if(i == 0) {
+				uniquePaper[i] = "White";
+			} else if(i == 1) {
+				uniquePaper[i] = "Gray";
+			} else if(i == 2) {
+				uniquePaper[i] = "Goldenrod";
+			}
+		}
+
+		for(int j = 0; j < uniqueInk.length; j++) {
+			if(j == 0) {
+				uniqueInk[j] = "Black";
+			} else if(j == 1) {
+				uniqueInk[j] = "Gray";
+			}
+		}
+	}
+
+
+
+	// ************ BEGIN INTERFACE FUNCTIONS ************
+
+	/* toString():  Return the String status of any object. */
+	public String toString() {
+
+		if(currentTick <= MAX_TIME_ALLOT) {
+			return "\n-----------------------\n" + "Current Simulation: Tick #" + currentTick +
+			" of " + MAX_TIME_ALLOT + "\n" +
+			generatedJobInformation + "\n";
+		} else {
+			return "\n------------------------\n" + "Current Simulation: Tick #" + currentTick +
+			" of " + MAX_TIME_ALLOT + "\n\t{ALERT} Simulation in overtime!\n" + "\tEstimated end: ";
+		}
+
+	}
+
+	/* update(): Done on essentially every tick. */
+	// NOTE:  update() for a Clock is stubbed.
+	public void update(){ }
+
+	// ************ END INTERFACE FUNCTIONS ************
+
+	public void setCurrentTick(int index) { currentTick = index; }
+	public static int getTotalSimTime() { return MAX_TIME_ALLOT; }
+	public static int getCurrentTick() { return currentTick; }
+	public static int getCurrentJob() { return currentJob; }
+	// How many jobs do you wish to burden-I MEAN do you wish to let me work on?
+	public static int generateNumberOfJobs() { return (int)(jobCount * Math.random()); }
+	public static int generateJobDuration() { return (int)(maxJobLength * Math.random() + 1); }
+
+	/* pause():  Pauses the simulation every "interval" ticks. */
+
+	public static void pause(int interval) {
+		if(currentTick % interval == 0 && interval > 0) {
+			Scanner input = new Scanner(System.in);
+			//String breakWord = "";
+			System.out.println("\t{PAUSE} Simulation has paused, every " + interval + " times.\n" +
+			"\tIf you wish to break out of the pause, please enter a number.\n");
+			while(!(input.hasNextInt())) {
+				// Put the entire simulation into an infinite loop until
+				// the user types a number.
+				
+             }
+            
+		}
+	}
+
+
+	/*  This method MUST be called from a loop to create each queue. */
+
+	public static Queue setUniqueQueue(int id, int i, int j) {
+		return new Queue(id, uniquePaper[i], uniqueInk[j]);
+	}
+
+	// Implementation not syntatically different than the regular Queue, I just had to change the signature.
+	public static PriorityQueue setUniqueQueue(PriorityQueue q, int id, int i, int j) {
+		return new PriorityQueue(id, uniquePaper[i], uniqueInk[j]);
+	}
+
+
+
+	/* Generate a job and place it in the queue in the same step (legal).  Uses either Queue type.*/
+
+	public void generateJob(Queue[] queues) {
+
+		Job[] orders;
+		String reply = "";
+		int jobs = generateNumberOfJobs();
+
+		if(jobs > 0) {
+			orders = new Job[jobs];
+			for(int i = 0; i < jobs; i++) {
+				currentJob++;
+				if(bias) {
+					orders[i] = new Job(bias); // Construct for biased jobs
+				} else if(priorities) { // Using Priority-switching schema
+					orders[i] = new Job(bias, WAIT_TIME); // Construct for impatient jobs
+				} else {
+					orders[i] = new Job(); // Normal construct for normal jobs
+				}
+				for(int j = 0; j < queues.length; j++) {
+					queues[j].enqueue(orders[i]);
+				}
+			}
+			for(int i = 0; i < orders.length; i++) {
+				reply += "\t" + orders[i].toString() + "\n";
+			}
+		}
+		generatedJobInformation = jobs + " job(s) generated this tick.\n" + reply;
+	}
+
+	public void generateJob(PriorityQueue[] queues) {
+
+		Job[] orders;
+		String reply = "";
+		int jobs = generateNumberOfJobs();
+
+		if(jobs > 0) {
+			orders = new Job[jobs];
+			for(int i = 0; i < jobs; i++) {
+				orders[i] = new Job(priorities, WAIT_TIME);
+				currentJob++;
+			}
+			for(PriorityQueue q: queues) {
+				for(Job j: orders) {
+					q.enqueue(j);
+				}
+			}
+			for(Job j: orders) {
+				reply += "\t" + j.toString() + "\n";
+			}
+		}
+		generatedJobInformation = jobs + " job(s) generated this tick.\n" + reply;
+	}
+
+
+
+	/* setPaperColor(), setInkColor():
+	   Both of these functions use the same algorithm to generate a random color paper for the queues.
+	   Six queues exist, so it will always match one of them.
+	*/
+
+	public static String setPaperColor(boolean biased) {
+
+		// Key: 30 numbers, 1-10 = white, 11-20 = gray, 21-30 = goldenrod
+		// Note:  75% of 30 is 22.
+		double key = (30 * (Math.random()) + 1);
+
+		if(biased) {
+
+			if(key >= 1 && key <=22) {
+				return "White";
+			} else if(key > 22 && key <= 26) {
+				return "Gray";
+			} else {
+				return "Goldenrod";
+			}
+		} else {
+			// Key: 30 numbers, 1-10 = white, 11-20 = gray, 21-30 = goldenrod
+
+			if(key >= 1 && key <=10) {
+				return "White";
+			} else if(key > 10 && key <= 20) {
+				return "Gray";
+			} else {
+				return "Goldenrod";
+			}
+		}
+	}
+
+	public static String setInkColor(boolean biased) {
+
+		// Key: 30 numbers, 1-22 = black, 23-30 = gray
+		// Note:  75% of 30 is 22.
+
+		double key = 30 * (Math.random()) + 1;
+		if(biased) {
+			if(key >= 1 && key <=22) {
+				return "Black";
+			} else {
+				return "Gray";
+			}
+		} else {
+			// Key: 30 numbers, 1-15 = black, 16-30 = gray
+
+			if(key >= 1 && key <=15) {
+				return "Black";
+			} else {
+				return "Gray";
+			}
+		}
+	}
+
+	// Just how important is a particular job?
+
+	public static int generatePriority() {
+		// Default priority is 3; if the other two numbers fall through, then Priority 3 will be returned.
+		int answer = 3;
+
+		double key = 30 * (randomizer) + 1;
+
+		if(key >= 0 && key < 10) {
+			answer = 1;
+		} else if(key >= 10 && key < 20) {
+			answer = 2;
+		}
+
+		return answer;
+	}
+
+	public void writeToFile(String file, String input) throws IOException {
+
+		try {
+			PrintWriter out = new PrintWriter(new FileWriter(file));
+			out.println("Jason Black, CS 2050 - Assignment #7, Printer Simulation\n");
+			out.println("Simulation stats:");
+			double ratio = getCurrentTick() / MAX_TIME_ALLOT;
+			out.println("\tSimulation ran for " + getCurrentTick() + "/" + MAX_TIME_ALLOT + " ticks; " +
+			ratio + " jobs were generated versus one job leaving the queue per tick.");
+			out.println("\tNumber of Jobs generated per tick: " + jobCount + "\n\tJob Length range: Between 1 and " +
+			maxJobLength + "\n\n");
+			out.println(input);
+
+			out.close();
+		} catch (IOException ioex) {
+			System.out.println("IOException occurred.  Program aborting.");
+			System.exit(0);
+		}
+	}
+}
---
+++ src/Printer.java
@@ -0,0 +1,561 @@
+/* Printer.java
+   This class is a "dumb" printer, able to get its status,
+   change its paper and ink, regurgitate its credentials,
+   and is able to tell you whether or not the configuration
+   is constant or not.  It can also attach to a printer
+   using a "symbolic link", or symlink, for short.
+   Most everything is "set" from the Clock class, meaning
+   that it gets its paper color and ink color from those.
+   Written by Jason Black, 23 October 2008
+*/
+
+import java.util.ArrayList;
+import java.text.DecimalFormat; // Formatting decimal places
+
+public class Printer implements ClockEvent {
+
+	protected int printer_ID, jobNumber, departureTick, arrivalTick, currentTick, changeoverTick;
+	protected int queue_ID, idleCounter, beginChangeTick, jobCounter, changes;
+	protected ArrayList<Integer> changelog;
+	protected boolean busy, hasQueue, isConstant, setPrintJob, hasBegun, impatient;
+	protected boolean settingInk, settingPaper, usingPriorityQueue;
+	protected final int INK_CHANGEOVER = 10, PAPER_CHANGEOVER = 5;
+	protected String paperColor, inkColor, output;
+	protected ArrayList<String> oldConfig, newConfig;
+	protected Queue currentQueue, softQueue;
+	protected PriorityQueue currentPriQueue, softPriQueue;
+	protected Job processingJobs;
+	protected ArrayList<Job> completeJobs;
+
+
+	public Printer(int id, boolean no_config_chg) {
+
+		this(id, Clock.setPaperColor(false), Clock.setInkColor(false));
+		isConstant = no_config_chg;
+	}
+
+	public Printer(int id, boolean no_config_chg, int paperSlots) {
+
+		this(id, Clock.setPaperColor(false), Clock.setInkColor(false));
+		isConstant = no_config_chg;
+		// Don't worry about paperSlots; required for construct in MultiPrinter
+	}
+
+	public Printer(boolean no_config_chg, int id) {
+		this(id, "White", "Black");
+		isConstant = no_config_chg;
+	}
+
+	public Printer(int id, boolean no_config_chg, boolean impatientJobs) {
+		this(id, no_config_chg);
+		impatient = impatientJobs;
+	}
+
+	protected Printer(int id, String paper, String ink) {
+
+		paperColor = paper;
+		inkColor = ink;
+		hasQueue = busy = setPrintJob = hasBegun = false;
+		printer_ID = id;
+		output = "";
+		queue_ID = jobCounter = changes = 0;
+		settingInk = settingPaper = usingPriorityQueue = false;
+		currentQueue = softQueue = null;
+		currentPriQueue = softPriQueue = null;
+		departureTick = arrivalTick = changeoverTick = 0;
+		currentTick = beginChangeTick = Clock.getCurrentTick();
+		completeJobs = new ArrayList<Job>(300);
+		changelog = new ArrayList<Integer>(100);
+		oldConfig = new ArrayList<String>(100);
+		newConfig = new ArrayList<String>(100);
+		processingJobs = null;
+
+	}
+
+	// ************ BEGIN INTERFACE FUNCTIONS ************
+
+	/* toString():  Return the String status of any object. */
+	@Override
+	public String toString() {
+		String info = "";
+		if(hasQueue) {
+			info = "Attached to Queue #" + queue_ID;
+		} else {
+			info = "not attached to any queue";
+		}
+		return "\tPrinter #" + printer_ID + " {" + paperColor + ", " + inkColor +
+		", " + info + "}";
+	}
+
+
+	/* update(): Done on essentially every tick. */
+	@Override
+	public void update() {
+
+		if(!(busy)) {
+			idleCounter++;
+		}
+
+		if(!(currentPriQueue == null) || !(softPriQueue == null)) {
+			usingPriorityQueue = true;
+		}
+
+		currentTick = Clock.getCurrentTick();
+		removeQueue();
+	}
+
+	// ************ END INTERFACE FUNCTIONS ************
+
+	public String getPaperColor() { return paperColor; }
+	public String getInkColor() { return inkColor; }
+	public String printStatus() { return output; }
+	public ArrayList<Job> getCompleteJobs() { return completeJobs; }
+	public int getChanges() { return changes; }
+	public int getJobsPrinted() { return jobCounter; }
+	public int getIdleTime() { return idleCounter; }
+	public Job getCurrentJob() { return processingJobs; }
+	public boolean hasJob() { return !(processingJobs == null); }
+	public boolean isBusy() { return busy; }
+	public boolean isAttached() { return hasQueue; }
+	// Flush most variables this way
+	protected void reset() {
+		hasQueue = busy = setPrintJob = false;
+		queue_ID = 0;
+		currentQueue = softQueue = null;
+		if(usingPriorityQueue) {
+			currentPriQueue  = softPriQueue = null;
+		}
+		processingJobs = null;
+	}
+	//Remove a queue from this Printer
+	public void removeQueue() {
+		if(currentQueue != null && processingJobs == null && currentQueue.canDetach()) {
+			currentQueue.detach();
+			output = "\t{WARNING} Queue #" + currentQueue.queue_ID +
+			" <{] [}> Printer #" + printer_ID + "\n";
+			currentQueue = softQueue = null;
+			hasQueue = false;
+			reset();
+		}
+
+		if(currentPriQueue != null && processingJobs == null && currentPriQueue.canDetach()) {
+			currentPriQueue.detach();
+			output = "\t{WARNING} PriorityQueue #" + currentPriQueue.queue_ID +
+			" <{] [}> Printer #" + printer_ID + "\n";
+			currentPriQueue = softPriQueue = null;
+			reset();
+		}
+	}
+	// symlink(Queue or PriorityQueue) symbolically links a Queue to a Printer.
+	public void symlink(Queue[] queues, boolean multiPrinting) {
+		
+		for(Queue q: queues) {
+
+            if(q.canSymlink()) {
+
+                String paper = q.getPaperColor();
+            	String ink = q.getInkColor();
+
+                if(inkColor.equals(ink) && paperColor.equals(paper)) {
+				
+                    hasQueue = true;
+                    currentQueue = q;
+                    softQueue = null;
+                    q.setPrinterID(printer_ID);
+                    queue_ID = q.queue_ID;
+                    q.setAttached();
+					if(!multiPrinting) {
+						output = "\t{NOTICE} " + q.toString() +
+						" <{]-[}> Printer #" + printer_ID + "; successful.\n";
+					} else {
+						output = "\t{NOTICE} " + q.toString() +
+						" <{]-[}> MultiPrinter #" + printer_ID + "; successful.\n";
+					}
+                    busy = false;
+                    break;
+                }
+            }
+		}
+	}
+	public void symlink(PriorityQueue[] queues) {
+		for(PriorityQueue q: queues) {
+			if(q.canSymlink()) {
+				//System.out.println("{MAKOTO} Symlinkable!");
+				String paper = q.getPaperColor();
+				String ink = q.getInkColor();
+				//System.out.println("{MAKOTO} Paper/Ink: " + paper + "/" + ink);
+				//System.out.println("{MAKOTO} Printer Paper/Ink: " + paperColor + "/" + inkColor);
+				if(ink.equals(inkColor) && paper.equals(paperColor)) {
+				//	System.out.println("{MAKOTO} Equivalent paper and ink color...");
+					q.setPrinterID(printer_ID);
+					queue_ID = q.queue_ID;
+					q.setAttached();
+					hasQueue = true;
+					currentPriQueue = q;
+					output = "\t{NOTICE} " + q.toString() +
+					" <{]-[}> Printer #" + printer_ID + "; successful.\n";
+					busy = false;
+					break;
+				}
+				//System.out.println("{MAKOTO} Something broke around here...");
+			}
+		}
+	}
+
+	//Print without a signature is used for PriorityQueues ONLY!
+	public void print() {
+
+		output = "";
+		if(!(setPrintJob)) {
+			if(!(currentPriQueue == null) && !busy) {
+				if(!(currentPriQueue.peek() == null) && currentPriQueue.peek().canPrint()) {
+					processingJobs = currentPriQueue.dequeue();
+					setPrintJob = true;
+					print(processingJobs);
+				} else {
+					removeQueue();
+				}
+			}
+		} else {
+			print(processingJobs);
+		}
+	}
+
+
+	public void print(boolean multiPrinting) {
+
+		output = "";
+		if(!(setPrintJob)) {
+			if(!(currentQueue == null) && !busy) {
+				if(!(currentQueue.peek(0) == null) && currentQueue.peek(0).canPrint()) {
+					processingJobs = currentQueue.dequeue();
+					setPrintJob = true;
+					print(processingJobs);
+				} else {
+					removeQueue();
+				}
+			}
+		} else {
+			print(processingJobs);
+		}
+	}
+	/*  The print() routine assumes that the job it's getting CAN print, so it won't question
+	    or check that, thanks to the canPrint() method.
+	*/
+	protected void print(Job j) {
+
+		output = "";
+		Job currentJob = j;
+		if(!(currentJob == null)) {
+
+			currentJob.update();
+			busy = true;
+			currentJob.declarePrinting();
+			// The job MUST be complete on tick C = (a + i + d), where i = idle time, d = duration,
+			// a = arrival time, and C = current tick
+			// Example:  Job arrives at 37, idle for 20, duration 6.  Will be done on 63.
+
+			if(!(hasBegun)) {
+				
+				output = "\t{STARTPRINT} Printer #" + printer_ID + " starts " +
+				currentJob.toString() + " this tick.\n";
+				hasBegun = true;
+			} else {
+				output = "\t{...PRINT} Printer #" + printer_ID + ", working on " +
+				currentJob.toString() + "\n";
+				
+			}
+
+			if(currentJob.getDepartureTime()-1 == Clock.getCurrentTick()) {
+				
+				output = "\t{ENDPRINT} Printer #" + printer_ID +
+				" completes Job #" + currentJob.getJobNumber() + " on this tick.\n";
+			}
+				completeJobs.add(processingJobs);
+				busy = hasBegun = setPrintJob = false;
+				processingJobs = null;
+				jobCounter++;
+			
+		} else {
+			output = "";
+		}
+	}
+	// Should be reasonable enough to state that a Queue won't link until both paper & ink match anyway
+	public void changeConfiguration(Queue[] queues, boolean multiPrinting) {
+
+		if(isConstant) { return; } // We don't want this printer to change if it's constant
+
+		if(softQueue == null) {
+			findQueue(queues);
+		} else if(softQueue.isEmpty()) { // Check to see if the queue went empty prematurely
+			softQueue = null;
+			changeConfiguration(queues, multiPrinting);
+		} else { // SoftQueue IS already set
+
+			String queue_paper = softQueue.getPaperColor();
+			String queue_ink = softQueue.getInkColor();
+			//System.out.println("Printer " + printer_ID + " " + queue_paper + " " + queue_ink);
+
+			if((!settingInk && !settingPaper) && !busy) {
+
+				//beginChangeTick = Clock.getCurrentTick();
+				if(!(inkColor.equals(queue_ink)) && paperColor.equals(queue_paper) && !settingInk) {
+
+					changeoverTick = INK_CHANGEOVER;
+					busy = true;
+					settingInk = true;
+
+				} else if(!(paperColor.equals(queue_paper)) && inkColor.equals(queue_ink)
+				&& !settingPaper) {
+					changeoverTick = PAPER_CHANGEOVER;
+					busy = true;
+					settingPaper = true;
+
+				} else if(!(paperColor.equals(queue_paper)) &&
+					   !(inkColor.equals(queue_ink)) && !settingPaper && !settingInk)  {
+					changeoverTick = (PAPER_CHANGEOVER + INK_CHANGEOVER);
+					busy = true;
+					settingPaper = settingInk = true;
+				} else if(paperColor.equals(queue_paper) && inkColor.equals(queue_ink)) {
+					return;
+				}
+			} else {
+				if(!multiPrinting) {
+					output = "\t{ALERT} Printer #: " + printer_ID  + " to change config on tick " +
+					(beginChangeTick + changeoverTick) + ", changing to match Queue #" + softQueue.queue_ID;
+				} else {
+					output = "\t{ALERT} MultiPrinter #: " + printer_ID  + " to change config on tick " +
+					(beginChangeTick + changeoverTick);
+					
+				}
+				//System.out.println(beginChangeTick);
+				//System.out.println(changeoverTick);
+				if(softQueue.isEmpty()) {
+					return;
+				} else if(Clock.getCurrentTick() == (beginChangeTick + changeoverTick)) {
+					changelog.add(Clock.getCurrentTick());
+					oldConfig.add("{" + paperColor + " & " + inkColor + "}");
+					inkColor = queue_ink;
+					paperColor = queue_paper;
+					newConfig.add("{" + paperColor + " & " + inkColor + "}");
+					busy = hasQueue = false;
+					currentQueue = softQueue;
+					if(!multiPrinting) {
+						output = "\t{NOTICE} Printer #" + printer_ID + " [}>-<{] " +
+						softQueue.toString() + "; successful.";
+					} else {
+						output = "\t{NOTICE} MultiPrinter #" + printer_ID + " [}>-<{] " +
+						softQueue.toString() + "; successful.";
+					}
+					softQueue.detach();
+					softQueue = null;
+					changes++;
+				}
+			}
+		}
+	}
+	protected void findQueue(Queue[] queues) {
+
+		if(impatient && !(hasQueue) && !(busy)) {
+			for(Queue q: queues) {
+				if(q.isAttached()) {
+					q.detach();
+					reset();
+				}
+				if(!(q.isEmpty()) && !(q.isSoftlinked()) && q.peek(0).getPriority() && q.printer_ID == 0) {
+					//reset();
+					softQueue = q;
+					softQueue.setSoftlinked();
+					softQueue.setPrinterID(printer_ID);
+					beginChangeTick = Clock.getCurrentTick();
+					break;
+				} else {
+					q.detach();
+					reset();
+				}
+			}
+			
+		} else if(!(hasQueue) && !(busy)) {
+				for(Queue q: queues) {
+					if(!(q.isEmpty()) && !q.isSoftlinked() && q.printer_ID == 0) {
+						softQueue = q;
+						softQueue.setSoftlinked();
+                        //softQueue.setAttached();
+						softQueue.setPrinterID(printer_ID);
+						beginChangeTick = Clock.getCurrentTick();
+						break;
+					} else {
+						q.detach();
+						reset();
+					}
+				}
+			}
+		}
+	
+	public void changeConfiguration(PriorityQueue[] queues) {
+
+		if(softPriQueue == null) {
+			findQueue(queues);
+		} else if(softPriQueue.isEmpty()) { // Check to see if the queue went empty prematurely
+			softPriQueue = null;
+			changeConfiguration(queues);
+		} else { // softPriQueue IS already set
+
+			String queue_paper = softPriQueue.getPaperColor();
+			String queue_ink = softPriQueue.getInkColor();
+
+
+			if((!settingInk && !settingPaper) && !busy) {
+
+				beginChangeTick = Clock.getCurrentTick();
+				if(!(inkColor.equals(queue_ink)) && paperColor.equals(queue_paper) && !settingInk) {
+
+					changeoverTick += INK_CHANGEOVER;
+					busy = true;
+					settingInk = true;
+
+				} else if(!(paperColor.equals(queue_paper)) && inkColor.equals(queue_ink)
+				&& !settingPaper) {
+
+					changeoverTick += PAPER_CHANGEOVER;
+					busy = true;
+					settingPaper = true;
+
+				} else if(!(paperColor.equals(queue_paper)) &&
+					   !(inkColor.equals(queue_ink)) && !settingPaper && !settingInk)  {
+
+					changeoverTick += (PAPER_CHANGEOVER + INK_CHANGEOVER);
+					busy = true;
+					settingPaper = settingInk = true;
+				}
+			} else {
+				output = "\t{ALERT} Printer #: " + printer_ID  + " to change config on tick " +
+				(beginChangeTick + changeoverTick);
+				//System.out.println(beginChangeTick);
+				//System.out.println(changeoverTick);
+				if(Clock.getCurrentTick() == (beginChangeTick + changeoverTick)) {
+					changelog.add(Clock.getCurrentTick());
+					oldConfig.add("{" + paperColor + " & " + inkColor + "}");
+					inkColor = queue_ink;
+					paperColor = queue_paper;
+					newConfig.add("{" + paperColor + " & " + inkColor + "}");
+					busy = false;
+					currentPriQueue = softPriQueue;
+					output = "\t{NOTICE} Printer #" + printer_ID + " [}>-<{] " +
+					currentPriQueue.toString() + "; successful.";
+					softPriQueue.detach();
+					softPriQueue = null;
+					changes++;
+					hasQueue = true;
+				}
+			}
+		}
+	}
+	protected void findQueue(PriorityQueue[] queues) {
+
+		if(!(hasQueue) && softPriQueue == null && !busy) {
+			for(PriorityQueue q: queues) {
+				if(q.canDetach()) {
+					q.detach();
+					reset();
+				}
+				if(!(q.isEmpty()) && processingJobs == null && !q.isSoftlinked()) {
+					softPriQueue = q;
+					queue_ID = softPriQueue.queue_ID;
+					softPriQueue.setSoftlinked();
+					softPriQueue.setPrinterID(printer_ID);
+					beginChangeTick = Clock.getCurrentTick();
+					break;
+				} else {
+					q.detach();
+					reset();
+				}
+			}
+		}
+	}
+	// Returns the total statistics of this printer.
+	public String getStats() {
+
+		DecimalFormat df = new DecimalFormat("###.##");
+
+		String foreword = "\n-------------------------------\n" +
+		"Printer #" + printer_ID +":\n\t - Idle for " + idleCounter + " total ticks\n\t" +
+		" - Changed configuration from ";
+
+		for(int i = 0; i < oldConfig.size(); i++) {
+				String s = oldConfig.get(i);
+				if(i == oldConfig.size() - 1) { // if we're on the last one, truncate " to ".
+					foreword += s;
+				} else {
+					foreword += s + " to ";
+				}
+		}
+		foreword += " on ticks: ";
+		for(int j = 0; j < changelog.size(); j++) {
+			int i = changelog.get(j);
+			foreword += "{" + i + "}";
+		}
+
+		foreword  += "\nJob readout for this Printer.\nJobs read from L-R, in processed order.\n" +
+		"EX: {Job #0, paper/ink, arrival, duration, [level]}\n";
+		String reply = "";
+		String[] jobReadout = new String[completeJobs.size()];
+		//System.out.println(jobReadout.length);
+		double avgIdle = 0, avgLength = 0;
+		//int i = 0;
+		Job j = null;
+		for(int i = 0; i < jobReadout.length; i++) {
+			j = completeJobs.get(i);
+			Integer priority = new Integer(0);
+			int arrival = j.getArrivalTime();
+			String level = "";
+			String inkColor = j.getInkColor();
+			String paperColor = j.getPaperColor();
+			if(j.getRealPriorityLevel() > 0) {
+				priority = j.getRealPriorityLevel();
+				level = ", " + Integer.toString(priority);
+			}
+			if(paperColor.equals("White")) {
+				paperColor = "Wht";
+			} else if(paperColor.equals("Goldenrod")) {
+				paperColor = "Gldnrd";
+			} else if(paperColor.equals("Gray")) {
+				paperColor = "Gry";
+			}
+			if(inkColor.equals("Black")) {
+				inkColor = "Blk";
+			} else if(inkColor.equals("Gray")) {
+				inkColor = "Gry";
+			}
+
+			jobReadout[i] = "{Job #" + j.getJobNumber() + ", " + paperColor +
+			"/" + inkColor + ", " + arrival + ", " + j.getDuration() + level + "}";
+			avgIdle += j.getIdleTime();
+			avgLength += j.getDuration();
+			j = null;
+		}
+
+		avgIdle /= completeJobs.size();
+		avgLength /= completeJobs.size();
+
+		reply += "\n\n";
+		for(int i = 0; i < jobReadout.length; i++) {
+			if(i % 3 == 0) {
+				reply += "\n";
+			}
+
+			reply += jobReadout[i] + "\t";
+			
+		}
+		String afterword = "";
+		afterword += "\n\n";
+		afterword += "Avg. Time Enqueued per Job: " +  df.format(avgIdle) + "\n" +
+		"Avg. Length per Job: " + df.format(avgLength) +
+		"\n" + "# of Jobs processed: " + completeJobs.size() + "\n" +
+		"# of Printer reconfigurations: " + changes;
+
+		return foreword  + reply + afterword;
+
+
+	}
+}
